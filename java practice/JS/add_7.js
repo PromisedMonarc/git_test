8ffcda37e272340b172a65bd511e83b301653a41
PromisedMonarc <mashaumukoki@gmail.com>
2025-02-03 17:07:10 +0200

    java practice/JS/add_7.js

--- java practice/JS/add_7.js
+++ java practice/JS/add_7.js
@@ -1,6 +1,9 @@
 function add7(x) {
     return x + 7;
+    console.log(add7);
 }
+
+add7();
 
 function mutliply(x, y) {
     return x * y;
---
+++ java practice/JS/happyBirthday.js
@@ -0,0 +1,7 @@
+function happybirthday (){
+    console.log("happy Birthday to you");
+    console.log("happy Birthday dearest you");
+
+}
+
+happybirthday();
