2dbfee33ef8d47670dd698f2223c57df55c1189c
PromisedMonarc <mashaumukoki@gmail.com>
2025-02-13 18:37:38 +0200

    jajanken/index.html jajanken/index.js jajanken/JaJanKen.md jajanken/style/style.css

--- jajanken/JS/index.js
+++ jajanken/JS/index.js
@@ -1,8 +1,25 @@
-function computerchoice() {
-    const choices = ("rock", "paper", "scissors");
-    const randomIndex = Math.floor(Math.random() * 3);
-    return choices(randomIndex)
-    console.log(choices);
+const choices = ["rock", "paper", "scissors"];
+const playerChoices = document.getElementById("playerChoices");
+const compChoices = document.getElementById("compChoices");
+const resultDisplay = document.getElementById("resultDisplay");
+
+function PlayGame(playerChoices){
+    const compChoices = choices[Math.floor(Math.random() * 3)];
+
+    console.log("player", playerChoices, "comp", compChoices);
+
+    if (playerChoices === compChoices) {
+        console.log("IT'S A TIE!");
+    } else if (
+        (playerChoices === "rock" && compChoices === "scissors")||
+        (playerChoices === "paper" && compChoices === "rock")||
+        (playerChoices === "scissors" && compChoices === "paper")
+     ) {
+        console.log(`You win! ${playerChoices} beats ${compChoices}`);
+    } else {
+        console.log(`You lose! ${compChoices} beats ${playerChoices}`);
+    }
+
+        console.log(compChoices);
 }
 
-computerchoice()
---
+++ jajanken/JaJanKen.md
@@ -0,0 +1,10 @@
+Computers choice
+- create computerchoice function
+- function should choose a random option
+
+Human choice
+- create humanchoice function
+- choose between the options
+
+Results
+- declare which player won
